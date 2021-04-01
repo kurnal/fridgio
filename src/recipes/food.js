ec9b96f32caf38779ae9af699f4e4fe16109a03b
Amanda Ma <amandama@Amandas-MacBook-Pro.local>
2021-04-01 12:50:17 -0400

    recipe changes

---
+++ src/recipes/food.js
@@ -0,0 +1,22 @@
+ var data = {
+    // "food": [
+    //     {
+            "name": "Mushroom Soup",
+            "cookingTime": "5 min",
+    //     },
+    //     {
+    //         "name":"broccoli",
+    //         "quantity": 3,
+    //         "category": "vegetable"
+    //     },
+    //     {
+    //         "name": "milk",
+    //         "quantity": 1, 
+    //         "category": "dairy"
+    //     }
+    //  ]
+}
+
+// var output = document.getElementById('output');
+// output.innerHTML = data.name;
+
